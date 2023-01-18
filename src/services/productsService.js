705e1ff1bb923322aedccbdd5aad743bae32bc88
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-01-18 19:50:20 -0300

    fix: importações Requisito 1

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -10,7 +10,7 @@ const getProduct = async (request) => {
   const products = await productsModel.getById(id);
   if (!products) {
  return {
-    status: 404, response: { message: 'Product not found' },
+    status: 404, message: 'Product not found',
   };
 }
   return { status: 200, message: products };
